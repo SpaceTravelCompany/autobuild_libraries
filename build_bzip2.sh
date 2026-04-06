dc6418ad747340bca5cbf5b3b2b6e16f47f13fb6
SeongJun <xfitgd@gmail.com>
2026-04-06 17:36:46 +0900

    1.6.2

--- build_bzip2.sh
+++ build_bzip2.sh
@@ -71,15 +71,15 @@ build_target() {
         )
     elif [ "$WINDOWS_ONLY" = true ]; then
         # CMake 3.12에서는 CMAKE_MSVC_RUNTIME_LIBRARY 적용이 불안정할 수 있어
-        # clang-cl 플래그로 /MT를 직접 강제한다.
+        # clang-cl 플래그로 정적 CRT(-MT)를 직접 강제한다.
         WIN_BASE_FLAGS="$(GET_WINDOWS_CLANG_TARGET_FLAG "${TARGET}") $(GET_WINDOWS_CLANG_CFLAGS "${TARGET}")"
         CMAKE_ARGS+=(
             -DENABLE_SHARED_LIB=ON
             -DENABLE_STATIC_LIB=ON
             -DCMAKE_C_COMPILER=clang-cl
             -DCMAKE_CXX_COMPILER=clang-cl
-            -DCMAKE_C_FLAGS="${WIN_BASE_FLAGS} /MT"
-            -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG"
+            -DCMAKE_C_FLAGS="${WIN_BASE_FLAGS} -MT"
+            -DCMAKE_C_FLAGS_RELEASE="-MT /O2 /Ob2 /DNDEBUG"
         )
     else
         CMAKE_ARGS+=(
